5db163c7261ac594d57bc9f5cf121703ae936d77
Bloodhaven Studios <106410531+BloodhavenStudios@users.noreply.github.com>
2024-02-26 10:55:21 -0600

    Create data.py

---
+++ data.py
@@ -0,0 +1,21 @@
+
+__all__: tuple[str, ...] = (
+    "return_data"
+)
+
+cookies = {
+    # Please get this data from the console network activity tool
+    # This is explained in the video :)
+}
+
+headers = {
+    # Please get this data from the console network activity tool
+    # This is explained in the video :)
+}
+
+params = {
+    "url": "dl"
+}
+
+def return_data() -> tuple[dict, dict, dict]:
+    return cookies, headers, params
