dc802bd9b81278fba2f2ecf4f353e77f8aceafa5
Bloodhaven Studios <106410531+BloodhavenStudios@users.noreply.github.com>
2024-03-17 13:46:41 -0500

    Update video_downloader.py
    Fix common error to where it auto creates a folder to download videos if you do not have one.

--- video_downloader.py
+++ video_downloader.py
@@ -3,6 +3,7 @@ from data import return_data
 from scraper_settings import ScraperSettings
 
 # Standard library imports
+import os
 from urllib.request import urlopen
 
 # Third-party library imports
@@ -13,6 +14,23 @@ from colorama import Fore
 __all__: tuple[str, ...] = (
     "download_video"
 )
+
+def save_video_as_mp4(response: str, link: str, id: int) -> None:
+    download_soup = BeautifulSoup(response, "html.parser")
+
+    download_link = download_soup.a["href"]
+    video_title = download_soup.p.getText().strip()
+
+    mp4_file = urlopen(download_link)
+    with open(f"{ScraperSettings.download_files_to}{id}-{video_title}.mp4", "wb") as output:
+        while(True):
+            data = mp4_file.read(4096)
+            if data:    
+                output.write(data)
+            else:
+                print("\r", end="")
+                print(f"Video download: {id} " + Fore.GREEN + "[Success]" + Fore.RESET)
+                break
 
 def download_video(link: str, id: int) -> None:
     """
@@ -32,22 +50,14 @@ def download_video(link: str, id: int) -> None:
     }
 
     response = requests.post('https://ssstik.io/abc', params=params, cookies=cookies, headers=headers, data=data)
-    download_soup = BeautifulSoup(response.text, "html.parser")
 
     try:
-        download_link = download_soup.a["href"]
-        video_title = download_soup.p.getText().strip()
-
-        mp4_file = urlopen(download_link)
-        with open(f"{ScraperSettings.download_files_to}{id}-{video_title}.mp4", "wb") as output:
-            while(True):
-                data = mp4_file.read(4096)
-                if data:    
-                    output.write(data)
-                else:
-                    print("\r", end="")
-                    print(f"Video download: {id} " + Fore.GREEN + "[Success]" + Fore.RESET)
-                    break
-    except:
+        save_video_as_mp4(response.text, link, id)
+    except FileNotFoundError:
+        os.mkdir("videos/")
+        ScraperSettings.download_files_to = "videos/"
+        save_video_as_mp4(response.text, link, id)
+    except Exception as e:
+        print(e)
         print("\r", end="")
         print(f"Video download: {id} " + Fore.RED + "[Failed]" + Fore.RESET + " (Try increasing ScraperSettings.threading_delay)")
